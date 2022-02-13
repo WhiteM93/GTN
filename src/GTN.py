288887042cc36a62fa498d573258bc9a278f5c5d
WhiteM93 <whitemax55@gmail.com>
2022-02-14 00:12:06 +0300

    Текущая проблема в том что цвет кнопки меняется только по контуру(почему то).

--- src/GTN.py
+++ src/GTN.py
@@ -5,13 +5,15 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
 
+
 class Main(Widget):
     pass
+
 
 class GTNApp(App):
     def build(self):
         return Main()
 
+
 if __name__ == '__main__':
     GTNApp().run()
-

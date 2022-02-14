83c44ec8b01080957c87b9da095a8ff58d7bb1be
WhiteM93 <whitemax55@gmail.com>
2022-02-14 03:48:35 +0300

    я устал и пошел спать

--- src/GTN.py
+++ src/GTN.py
@@ -4,11 +4,41 @@
 
 from kivy.app import App
 from kivy.uix.widget import Widget
+from kivy.properties import ObjectProperty
+from kivy.clock import Clock
+import random
+import time
+
+
+score = 0
+
 
 
 class Main(Widget):
-    pass
+    datainput = ObjectProperty()
+    answer = ObjectProperty()
+    enter = ObjectProperty()
+    score = ObjectProperty()
+    records = ObjectProperty()
+    help = ObjectProperty()
+    score1 = 1
 
+    def update_data1(self):
+        self.data1.text = str(time.ctime())
+        Clock.schedule_interval(self.Callback_Clock, 1)
+
+
+    def Callback_Clock(self, dt):
+        self.data1.text = str(time.ctime())
+
+
+    def random_number(self):
+        rm = str(random.randint(1,10))
+        self.score.text = str(score)
+        if self.datainput.text == rm:
+            self.answer.text = 'Верно ' + str(rm)
+        else:
+            self.answer.text = 'Неверно ' + str(rm)
 
 class GTNApp(App):
     def build(self):
